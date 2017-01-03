26ed2a02be76041423dc53584e587b6cea895585
Tim Barland <Tim Barland>
2017-01-03 17:23:59 -0500

    Added tests for deleting users

---
+++ test/delete_test.js
@@ -0,0 +1,50 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Deleting a user', () => {
+    let joe;
+
+    beforeEach((done) =>{
+      joe = new User({ name: 'Joe'});
+      joe.save()
+        .then(() => done());
+    });
+
+    it('model instance remove', (done) => {
+      // removes just the one instance
+      joe.remove()
+        .then(() => User.findOne({ name: 'Joe'}))
+        .then((user) => {
+          assert(user === null);
+          done();
+        });
+    });
+
+    it('class method remove', (done) => {
+      // removes every instance where name = Joe
+      User.remove({ name: 'Joe'})
+        .then(() => User.findOne({ name: 'Joe'}))
+        .then((user) => {
+          assert(user === null);
+          done();
+        });
+    });
+
+    it('class method findOneAndRemove', (done) => {
+      User.findOneAndRemove({ name: 'Joe' })
+        .then(() => User.findOne({ name: 'Joe'}))
+        .then((user) => {
+          assert(user === null);
+          done();
+    });
+  });
+
+    it('class method findByIdAndRemove', (done) => {
+      User.findByIdAndRemove(joe._id)
+        .then(() => User.findOne({ name: 'Joe'}))
+        .then((user) => {
+          assert(user === null);
+          done();
+    });
+  });
+});
--- test/reading_test.js
+++ test/reading_test.js
@@ -18,4 +18,12 @@ describe('Reading records', () => {
         done();
       });
   });
+
+  it('find a user with a particular id', (done) => {
+    User.findOne({ _id: joe._id})
+      .then((user) => {
+        assert(user.name === 'Joe');
+        done();
+      });
+  });
 });
