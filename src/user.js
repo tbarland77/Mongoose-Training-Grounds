723bc44156fa74b2888fd38bceefe93514929ce0
Tim Barland <Tim Barland>
2017-01-06 16:39:31 -0500

    Added tests for validating records

--- src/user.js
+++ src/user.js
@@ -3,7 +3,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema ({
-  name: String
+  name: {
+    type: String,
+    validate: {
+      validator: (name) => name.length > 2,
+      message: 'Name must be longer then 2 characters.'
+    },
+    required: [true, 'Name is required']
+  },
+  postCount: Number
 });
 // represents entire collection of users not just a single user
 const User = mongoose.model('user', UserSchema);
--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating a user', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe'});
+    joe = new User({ name: 'Joe', postCount: 0});
     joe.save()
       .then(() => done());
   });
@@ -48,5 +48,14 @@ describe('Updating a user', () => {
       User.findByIdAndUpdate(joe._id, { name: 'Alex'}),
       done
     );
+  });
+
+  it('A user can have their postcount incremented by 1', (done) => {
+    User.update({ name: 'Joe'}, {$inc: {postCount: 1}})
+      .then(() => User.findOne({ name: 'Joe'}))
+      .then((user) => {
+        assert(user.postCount === 1);
+        done();
+      })
   });
 });
